30d79a8676a57d7185670369eefc92e3021a8e1f
jedediahriley <46664082+jedediahriley@users.noreply.github.com>
2021-04-04 17:41:17 -0400

    updated server.pl

--- server.pl
+++ server.pl
@@ -1,5 +1,30 @@
-use Mojolicious::Lite;
+use Mojolicious::Lite -signatures;
 
-get '/' => {text => 'I ♥ Mojolicious!'};
+# Render template "index.html.ep" from teh DATA section
+get '/' => sub ($c) {
+    $c->render(template => 'index')
+};
+
+# Websocket service used by the template to extract the title from a website
+websocket '/title' => sub ($c) {
+    $c->on(message => sub ($c, $msg) {
+        my $title = $c->get($msg)->result->dom->at('title')->text;
+        $c -> ($title);
+    });
+};
 
 app->start;
+
+__DATA__
+
+@@ index.html.ep
+
+% my $url = url_for 'title';
+
+<script>
+    var ws = new Websocket('<%= $url->to_abs %>');
+    ws.onmessage = function (event) {document.body.innerHtml += event.data};
+    ws.onopen    = function (event) {ws.send('https://mojolicious.org')};
+</script>
+
+    <p>hello world
